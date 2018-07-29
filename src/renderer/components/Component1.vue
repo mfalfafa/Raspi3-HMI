efe1393807bdf6c6a620ad984b0129b24abdc7bf
FF077 <miftahf77@gmail.com>
2018-07-29 15:09:51 +0700

    HMI for Raspi 3

---
+++ src/renderer/components/Component1.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <h2>{{component_name}}</h2>
+    <h2>{{method01()}}</h2>
+    <h2>{{method02()}}</h2>
+    <button v-on:click="count++">You click me {{count}}</button>
+</div>
+</template>
+
+<script>
+    export default {
+        name: 'component1',
+        data(){
+            return {
+                component_name:"This is Component 1",
+                count:0,
+                items:[
+                    {message:"a"},
+                    {message:"b"}
+                ]
+            }
+        },methods:{
+            method01: function(){
+                return "Hello, this is my own component 1.";
+            },
+            method02: function(){
+                return "This create by vue javascript";
+            },
+            method03: function(){
+                this.component_name = '=== Component 01 ==='
+            }
+        }
+
+    }
+</script>
